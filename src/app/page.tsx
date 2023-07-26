e099afa2d7efd4801f6b4dbe97b726285466c93a
jim <jag.arnold.go@gmail.com>
2023-07-26 11:28:08 -0700

    add min max for anual interest rate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,6 +96,8 @@ export default function Home() {
                     name="annual-interest-rate"
                     value={interestRate}
                     step={0.001}
+                    min={0}
+                    max={100}
                     onChange={(e) => setInterestRate(parseInt(e.target.value))}
                     className="block pl-1.5 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
