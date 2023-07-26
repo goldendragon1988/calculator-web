2bb4144066b637300228e75bde11be7f7907ffdb
jim <jag.arnold.go@gmail.com>
2023-07-26 11:14:40 -0700

    fix step for numbers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,6 +95,7 @@ export default function Home() {
                     type="number"
                     name="annual-interest-rate"
                     value={interestRate}
+                    step={0.1}
                     onChange={(e) => setInterestRate(parseInt(e.target.value))}
                     className="block pl-1.5 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
